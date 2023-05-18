6c869bda97c7714a0cde880d179a8388dea562ac
Levente Nagy <113554470+Levkol@users.noreply.github.com>
2023-05-18 07:28:06 +0200

    Coding Challenge 5

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 'use strict';
 
-// Data needed for a later exercise
-// const flights =
-//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
 // Data needed for first part of the section
 const weekdays = [`mon`, `tue`, `wed`, `thu`, `fri`, `sat`, `san`];
 
@@ -942,6 +938,7 @@ for (const [minutes, events] of gameEvents) {
 }
 */
 
+/*
 ///////////////////////////////////////////////////////
 // Coding Challenge 4:
 /*
@@ -973,11 +970,11 @@ Afterwards, test with your own test data!
 
 GOOD LUCK 😀
 
-*/
+
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
 
-document.querySelector(`button`).addEventListener(`click`, function (words) {
+document.querySelector(`button`).addEventListener(`click`, function () {
   const text = document.querySelector(`textarea`).value;
   const rows = text.split(`\n`);
 
@@ -990,3 +987,27 @@ document.querySelector(`button`).addEventListener(`click`, function (words) {
     console.log(`${output.padEnd(20, ` `)}${`✅`.repeat(i + 1)}`);
   }
 });
+*/
+
+///////////////////////////////////////////////////////
+// String  Methods Practice
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+//  🔴 Delayed Departure from FAO to TXL (11h25)
+//               Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//             Departure from FAO to LIS (12h30)
+const getCode = str => str.toUpperCase().slice(0, 3);
+
+for (const flight of flights.split(`+`)) {
+  const [type, from, to, time] = flight.split(`;`);
+  const output = `${type.startsWith(`_Delayed`) ? `🔴` : ``}${type.replaceAll(
+    `_`,
+    ` `
+  )} from ${getCode(from)} to ${getCode(to)} (${time.replace(
+    `:`,
+    `h`
+  )})`.padStart(45);
+  console.log(output);
+}
